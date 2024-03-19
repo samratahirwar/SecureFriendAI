2aae5bfabc54f41639ade48df0af75e4c6df3e5b
Samrat Ahirwar <93487468+samratahirwar@users.noreply.github.com>
2024-03-19 13:27:53 +0530

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$d5c5h&vkazayd$nevzv2%xrexgv9jv0rauzx(+ue^-0)##&ly'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =  ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
